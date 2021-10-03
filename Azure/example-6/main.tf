f9618cae4ed4d6ede3099521da4709997da60927
Arun K Singh <22137632+arunksingh16@users.noreply.github.com>
2021-10-03 17:26:06 +0100

    Update main.tf

--- Azure/example-6/main.tf
+++ Azure/example-6/main.tf
@@ -1,1 +1,38 @@
+#############################################################################
+# TERRAFORM CONFIG
+#############################################################################
 
+terraform {
+  backend "azurerm" {
+        resource_group_name  = "rg-terraform"
+        storage_account_name = "XXXXXXXXXX"
+        container_name       = "terraform"
+        key                  = "aks.vnet.terraform.tfstate"
+    }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+
+}
+
+provider "kubernetes" {
+  host                   = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
+  username               = "${azurerm_kubernetes_cluster.k8s.kube_config.0.username}"
+  password               = "${azurerm_kubernetes_cluster.k8s.kube_config.0.password}"
+  client_certificate     = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)}"
+  client_key             = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)}"
+}
