cf9fd4e9f05a36853846b81bc9d2eee61c832a1d
Arun K Singh <22137632+arunksingh16@users.noreply.github.com>
2021-10-03 17:27:22 +0100

    Create aks.tf

---
+++ Azure/example-6/aks.tf
@@ -0,0 +1,48 @@
+
+resource "azurerm_kubernetes_cluster" "test" {
+  name       = var.aks_name
+  location   = data.azurerm_resource_group.rg.location
+  dns_prefix = var.aks_dns_prefix
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  linux_profile {
+    admin_username = var.vm_user_name
+
+    ssh_key {
+      key_data = file(var.public_ssh_key_path)
+    }
+  }
+# HTTP Application Routing need to be disabled
+  addon_profile {
+    http_application_routing {
+      enabled = false
+    }
+  }
+
+  default_node_pool {
+    name            = "agentpool"
+    node_count      = var.aks_agent_count
+    vm_size         = var.aks_agent_vm_size
+    os_disk_size_gb = var.aks_agent_os_disk_size
+    vnet_subnet_id  = data.azurerm_subnet.kubesubnet.id
+    # dns_prefix     MISSING
+  }
+
+  service_principal {
+    client_id     = var.aks_service_principal_app_id
+    client_secret = var.aks_service_principal_client_secret
+  }
+
+  network_profile {
+    network_plugin     = "azure"
+    dns_service_ip     = var.aks_dns_service_ip
+    docker_bridge_cidr = var.aks_docker_bridge_cidr
+    service_cidr       = var.aks_service_cidr
+  }
+
+  depends_on = [
+    azurerm_virtual_network.test,
+    azurerm_application_gateway.network,
+  ]
+  tags = var.tags
+}
