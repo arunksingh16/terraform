a1297422106d4d843e8b8d6036e960665a0071a6
Arun K Singh <22137632+arunksingh16@users.noreply.github.com>
2021-10-05 16:51:46 +0100

    Update main.tf

--- AWS/example-1/main.tf
+++ AWS/example-1/main.tf
@@ -1,1 +1,11 @@
+output "azname-1" {
+  value = "${data.aws_availability_zones.available.names[0]}"
+}
 
+output "azname-2" {
+  value = "${data.aws_availability_zones.available.names[1]}"
+}
+
+output "ami-1" {
+  value = "${data.aws_ami.amazon_linux.id}"
+}
