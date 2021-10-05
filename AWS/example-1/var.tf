8aa7783d7cfdbd470aa02f4133fd21fe325acb27
Arun K Singh <22137632+arunksingh16@users.noreply.github.com>
2021-10-05 16:52:29 +0100

    Create var.tf

---
+++ AWS/example-1/var.tf
@@ -0,0 +1,16 @@
+##########################
+#    VARIABLES
+##########################
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-east-1"
+}
+
+variable "aws_access_key_id" {
+  default = "xxxxx"
+}
+
+variable "aws_secret_access_key" {
+  default = "xxxxxxxxxxxxxxx"
+}
+
