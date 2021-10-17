ca006fbcc8ac6d6a937e37e5ba6dae55590db9d6
Arun K Singh <22137632+arunksingh16@users.noreply.github.com>
2021-10-17 19:10:23 +0100

    Update main.tf

--- AWS/example-3/main.tf
+++ AWS/example-3/main.tf
@@ -1,1 +1,49 @@
+##########################
+#    PROVIDER
+##########################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = var.aws_region
+  access_key= var.aws_access_key_id
+  secret_key= var.aws_secret_access_key
+}
+
+##########################
+#    VARIABLE
+##########################
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-east-1"
+}
+
+variable "aws_access_key_id" {
+  default = "XXXXXX"
+}
+
+variable "aws_secret_access_key" {
+  default = "XXXXXX"
+}
+
+##########################
+#    RESOURCE
+##########################
+
+resource "aws_iam_user" "iam_accnt" {
+  count = 3
+  name  = "accnt-${count.index}"
+}
 
